16f00fae802ccd71a35e0f9e72d0c1913817bee7
sikutisa <ilgooinkorea@gmail.com>
2024-08-03 13:09:01 +0900

    update apphost

--- AspireYouTubeSummariser/AspireYouTubeSummariser.AppHost/Program.cs
+++ AspireYouTubeSummariser/AspireYouTubeSummariser.AppHost/Program.cs
@@ -3,6 +3,7 @@ var builder = DistributedApplication.CreateBuilder(args);
 var config = builder.Configuration;
 
 var backend = builder.AddProject<Projects.backend>("backend")
+                     .WithExternalHttpEndpoints()
                     .WithEnvironment("OpenAI__Endpoint", config["OpenAI:Endpoint"])
                     .WithEnvironment("OpenAI__ApiKey", config["OpenAI:ApiKey"])
                     .WithEnvironment("OpenAI__DeploymentName", config["OpenAI:DeploymentName"]);
